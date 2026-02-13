5e1ffce329d2c47f9c833a7d62cdca2619fbb506
Nick <62498411+TesterULife@users.noreply.github.com>
2026-02-13 18:54:41 +0700

    from v10

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -12,18 +12,24 @@ def test_getting_users(get_users, make_number):
 @pytest.mark.production
 @pytest.mark.skip('For future updates')
 def test_another():
-    assert 7 == 7
+    """
+    In this test we try to check is 1 yo equal to 7
+    """
+    assert 1 == 7
 
 @pytest.mark.development
 @pytest.mark.parametrize(
     'first_value, second_value, result',[
         (1, 2, 3),
         (-1, 2, 1),
-        (-7, -2, -9),
+        (-7, -2, -5),
         ('a', 2, None),
         ('b', 'c', 'bc')
 
 ])
 
 def test_calculator(first_value, second_value, result, calculate):
+    """
+    In this test we are testing the calculation with different values
+    """
     assert calculate(first_value, second_value) == result
